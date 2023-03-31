fc7ba9842af5c94e132eca853dd8302854877cd8
n0taaryav <128344052+n0taaryav@users.noreply.github.com>
2023-03-31 15:45:24 +0530

    Add files via upload

---
+++ udemy-python/Projects/project1.py
@@ -0,0 +1,5 @@
+int_1 = 4
+float_1 = 32.4
+bool_1 = False
+
+int_1 = 5
---
+++ udemy-python/rough_video_code.py
@@ -0,0 +1,86 @@
+#variables
+ex_var = 5
+tjkd22 = 4
+
+#data types
+float = 1.3
+int = 4
+bool = True
+
+#comments
+
+# comments are fun
+
+#math operators
+addition = 1+1 #2
+substraction = 2-1 #1
+multiplication = 3*2 #6
+division = 4/2 #2
+
+exponentiation = 4**4 #256
+floor_divison = 16 //5 #3
+modulo = 7 %3 #1
+
+#assaignment operators
+add_assaign = 5
+add_assaign += 7
+
+sub_assaign = 5
+sub_assaign -= 13
+
+mul_assaign = 5
+mul_assaign *= 15
+
+div_assaign = 5
+div_assaign /= 20
+
+#print
+example = 2.1
+example2 = 2
+example3 = False
+
+print(example)
+print(example2)
+print(example3)
+
+print(2019)
+
+print((4+5) * 3)
+
+#more on floats
+exp = (123+280)/100
+print(exp)
+
+#round
+exp3 = 2.12+3.13
+print(round(exp,2))
+
+#strings
+ex_1 = "hello" + 'world'
+ex_2 = "1230"
+ex_3 = "$#%^&@*(#)"
+ex_4 = "Hello World"
+print(ex_4[1])
+print("apple"[2])
+
+#string slicing
+
+sliceing_ex = "Oranges"
+
+print(sliceing_ex[:3])
+print(sliceing_ex[2:5])
+print(sliceing_ex[4:])
+
+#concatination
+
+print("apple" + " " + "pie" )
+
+ex_41 = "AB" + " CD" + "EF"
+print(ex_41[:2])
+print(ex_41[3:6])
+
+unchanged = "Hello world"
+sliced = unchanged[:4]
+print(sliced)
+print(unchanged)
+    
